84da56eba563825496f98b9cb940d2a067531277
anavick <freitasgabrielanasilva061325@gmail.com>
2023-05-12 11:45:08 -0300

    primeiro commit do projeto react

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import Soma from "./components/Soma";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>projeto em react por Ana victoria</h1>
+      <Soma></Soma>
+    </div>
+    
+  );
+}
+
+export default App;
+
+
---
+++ my-app/src/components/Soma.tsx
@@ -0,0 +1,12 @@
+function Soma(){
+    return(<h1>função de soma</h1>);
+}
+
+// 3 regras essenciais 
+//todo componente precisa estar dentro de algum HTML
+//todo componente precisa começar com a letra maiuscula
+//sempre exportar para usar
+export default Soma;
+
+
+// TAREFA: criar 2 caixas de texto, criar 1 botao, somar os 2 valores e exibir em um alert e depois no html em algum lugar
